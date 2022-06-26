27dd9341203bc3ac7745203f262c7cf3a37df8ea
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2022-06-27 08:22:28 +1000

    Project Ready for 22T2

---
+++ src/main/java/dungeonmania/DungeonManiaController.java
@@ -0,0 +1,77 @@
+package dungeonmania;
+
+import dungeonmania.exceptions.InvalidActionException;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.util.Direction;
+import dungeonmania.util.FileLoader;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DungeonManiaController {
+    public String getSkin() {
+        return "default";
+    }
+
+    public String getLocalisation() {
+        return "en_US";
+    }
+
+    /**
+     * /dungeons
+     */
+    public static List<String> dungeons() {
+        return FileLoader.listFileNamesInResourceDirectory("dungeons");
+    }
+
+    /**
+     * /configs
+     */
+    public static List<String> configs() {
+        return FileLoader.listFileNamesInResourceDirectory("configs");
+    }
+
+    /**
+     * /game/new
+     */
+    public DungeonResponse newGame(String dungeonName, String gameMode) throws IllegalArgumentException {
+        return null;
+    }
+
+    /**
+     * /game/dungeonResponseModel
+     */
+    public DungeonResponse getDungeonResponseModel() {
+        return null;
+    }
+
+    /**
+     * /game/tick/item
+     */
+    public DungeonResponse tick(String itemUsedId) throws IllegalArgumentException, InvalidActionException {
+        return null;
+    }
+
+    /**
+     * /game/tick/movement
+     */
+    public DungeonResponse tick(Direction movementDirection) {
+        return null;
+    }
+
+    /**
+     * /game/build
+     */
+    public DungeonResponse build(String buildable) throws IllegalArgumentException, InvalidActionException {
+        return null;
+    }
+
+    /**
+     * /game/interact
+     */
+    public DungeonResponse interact(String entityId) throws IllegalArgumentException, InvalidActionException {
+        return null;
+    }
+}
