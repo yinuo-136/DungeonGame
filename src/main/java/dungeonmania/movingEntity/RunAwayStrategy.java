b67cf521154d80d47c7991f4bf2ae0f84d537250
Raphael <z5360132@ad.unsw.edu.au>
2022-07-12 17:24:49 +1000

    add getEntitiesByPosition function

---
+++ src/main/java/dungeonmania/movingEntity/RunAwayStrategy.java
@@ -0,0 +1,13 @@
+package dungeonmania.movingEntity;
+
+public class RunAwayStrategy implements MercenaryMovingStrategy {
+    public void move() {
+        
+    }
+    
+    public String getStatename() {
+        return "RunAway";
+    }
+    
+}
+    
