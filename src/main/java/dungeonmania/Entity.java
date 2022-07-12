8f61da82155a8a831148e377e8bd032844dfd875
Raphael <z5360132@ad.unsw.edu.au>
2022-07-12 21:53:47 +1000

    command out line that cause error in entity

--- src/main/java/dungeonmania/Entity.java
+++ src/main/java/dungeonmania/Entity.java
@@ -1,12 +1,14 @@
 package dungeonmania;
 
 import dungeonmania.response.models.EntityResponse;
+import dungeonmania.util.Position;
 
 public abstract class Entity {
     protected DungeonInfo dungeonInfo;
     public abstract String getId();
     public abstract String getType();
     public abstract EntityResponse getEntityResponse();
+    //public abstract Position getPosition();
     
     public void setDungeonInfo(DungeonInfo dungeonInfo) {
         this.dungeonInfo = dungeonInfo;
