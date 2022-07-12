25c89da6f69b2733142ddf4947c58caadb1bdb08
Jay Sern Saw <sawjaysern@Jays-MacBook-Air.local>
2022-07-12 17:39:04 +1000

    merge conflicts solved

--- src/main/java/dungeonmania/movingEntity/Moving.java
+++ src/main/java/dungeonmania/movingEntity/Moving.java
@@ -7,11 +7,7 @@ import dungeonmania.util.Position;
 public interface Moving {
     public void attack(Player player);
     public Position getPosition();
-<<<<<<< HEAD
-    public double getHealth();
-=======
     public String getType();
     public double getHealth();
     public int getDamage();
->>>>>>> master
 }
