04ee22ed9e7fac820b27db0630a4f380d91bfb9e
Ian Hou <z5324093@ad.unsw.edu.au.com>
2022-07-14 15:11:06 +1000

    finish spawn features

--- src/main/java/dungeonmania/movingEntity/ZombieToast.java
+++ src/main/java/dungeonmania/movingEntity/ZombieToast.java
@@ -26,8 +26,12 @@ public class ZombieToast extends Entity implements Moving {
     }
 
     public void setConfig(){
-        this.health = dungeonInfo.getSpecificConfig("zombie_health");
-        this.damage = dungeonInfo.getSpecificConfig("zombie_attack");
+    }
+
+    
+
+    public void setDamage(int damage) {
+        this.damage = damage;
     }
 
     public void move() {
