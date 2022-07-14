ee35bc43563496652f59bc683714d3ff098aef8f
Raphael <z5360132@ad.unsw.edu.au>
2022-07-15 01:05:02 +1000

    finished runaway strategy

--- src/main/java/dungeonmania/movingEntity/RunAwayStrategy.java
+++ src/main/java/dungeonmania/movingEntity/RunAwayStrategy.java
@@ -1,8 +1,14 @@
 package dungeonmania.movingEntity;
 
+import java.util.Arrays;
+import java.util.List;
+
 import dungeonmania.Entity;
+import dungeonmania.util.Direction;
 
 public class RunAwayStrategy implements MercenaryMovingStrategy {
+    private List<String> movingConstrintItemList = Arrays.asList("wall", "boulder");
+
     
     public String getStatename() {
         return "RunAway";
@@ -10,8 +16,18 @@ public class RunAwayStrategy implements MercenaryMovingStrategy {
 
     @Override
     public void move(Entity movingEntity) {
+        DijkstraAlgoPathFinder pathFinder = new DijkstraAlgoPathFinder();
+        Direction direction = pathFinder.findNextPathAwayPlayer(movingEntity);
+        if (direction == null) {
+            return;
+        }
+        movingEntity.setPos(movingEntity.getPos().translateBy(direction));
         
-        
+    }
+
+    public boolean checkObstacle(Moving entity, Direction direction) {
+        // check if that direction of entity is a obstacle, return true if it is and vice versa
+        return entity.getEntitiesStringByPosition(entity.getPos().translateBy(direction)).stream().anyMatch(element -> movingConstrintItemList.contains(element));
     }
     
 }
