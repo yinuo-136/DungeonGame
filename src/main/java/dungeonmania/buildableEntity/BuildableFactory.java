34e970f475a389635f6e04a534fbbc9f82b12579
Usman Hanif <u.hanif@student.unsw.edu.au>
2022-07-15 20:05:03 +1000

    ReAdd Buildable factory

---
+++ src/main/java/dungeonmania/buildableEntity/BuildableFactory.java
@@ -0,0 +1,10 @@
+package dungeonmania.buildableEntity;
+
+import dungeonmania.DungeonInfo;
+import dungeonmania.exceptions.InvalidActionException;
+
+public class BuildableFactory {
+    public void build(String buildable, DungeonInfo info) throws IllegalArgumentException, InvalidActionException {
+
+    }
+}
