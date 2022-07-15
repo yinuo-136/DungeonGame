1e11e7545d2bc8e3a6ffcd0c00ed1a09b5b22129
Usman Hanif <u.hanif@student.unsw.edu.au>
2022-07-15 21:16:13 +1000

    Finish Shield Crafting

--- src/main/java/dungeonmania/buildableEntity/Shield.java
+++ src/main/java/dungeonmania/buildableEntity/Shield.java
@@ -1,5 +1,7 @@
 package dungeonmania.buildableEntity;
 
+import java.util.List;
+
 import dungeonmania.DungeonInfo;
 import dungeonmania.exceptions.InvalidActionException;
 import dungeonmania.inventoryItem.InvItem;
@@ -38,7 +40,26 @@ public class Shield implements InvItem,Buildable {
     }
 
     public void craft(DungeonInfo info) throws InvalidActionException {
-        //TODO
+        List<String> woodIdList = info.getInvItemIdsListByType("wood");
+        List<String> treasureIdList = info.getInvItemIdsListByType("treasure");
+        List<String> keyIdList = info.getInvItemIdsListByType("key");
+         
+
+        if ((woodIdList.size()<2) || ((treasureIdList.size()<1) && (keyIdList.size()<1)) ) {
+            throw new InvalidActionException("Insufficient Materials to craft Shield!");
+        }
+
+        //Remove one wood from inventory
+        info.removeInvItemById(woodIdList.get(0));
+        info.removeInvItemById(woodIdList.get(1));
+
+        //Remove Treasure or Key from Inventory (Give priority to Treasure)
+        if (treasureIdList.size() < 1) {
+            info.removeInvItemById(keyIdList.get(0));
+        } else {
+            info.removeInvItemById(treasureIdList.get(0));
+        }
+        
     }
 
     public Boolean isItemDestroyed() {
