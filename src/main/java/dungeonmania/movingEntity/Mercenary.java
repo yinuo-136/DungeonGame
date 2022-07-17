e2eb63949056d0a6b951873a7d109b8289e8b9ef
Raphael <z5360132@ad.unsw.edu.au>
2022-07-17 13:40:55 +1000

    merge with master

--- src/main/java/dungeonmania/movingEntity/Mercenary.java
+++ src/main/java/dungeonmania/movingEntity/Mercenary.java
@@ -20,7 +20,6 @@ public class Mercenary extends Entity implements Moving {
     private int bribeRadius;
     private String type = "mercenary";
     private boolean bribed = false;
-    private Position playerLastPosition;
 
     public Mercenary(Position position, String id) {
         this.position = position;
@@ -36,7 +35,6 @@ public class Mercenary extends Entity implements Moving {
 
     public void move() {
         currentState.move(this);
-        playerLastPosition = getDungeonInfo().getPlayer().getPos();
     }
     
     @Override
