dd0ff40a324b55d875915e89339402d0b4a1909b
sneaky-patriki <n.patrikeos@student.unsw.edu.au>
2022-07-28 23:26:03 +1000

    Milestone 3 Dryrun Results

--- src/main/java/dungeonmania/goal/Goal.java
+++
@@ -1,5 +0,0 @@
-package dungeonmania.goal;
-
-public interface Goal {
-    public String evalGoal(); // get what goals are still not achieved via String.
-}
--- src/main/java/dungeonmania/movingEntity/ZombieType.java
+++
@@ -1,6 +0,0 @@
-package dungeonmania.movingEntity;
-
-public interface ZombieType {
-    public void setStrategy(MercenaryMovingStrategy strategy);
-    public void revertStrategy();
-}
