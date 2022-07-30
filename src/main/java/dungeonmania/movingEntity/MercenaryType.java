eb37fdefe11f581119f318503c33173de21e83aa
Usman Hanif <u.hanif@student.unsw.edu.au>
2022-07-31 03:12:02 +1000

    Implement Mind Control into Mercenary Types

--- src/main/java/dungeonmania/movingEntity/MercenaryType.java
+++ src/main/java/dungeonmania/movingEntity/MercenaryType.java
@@ -1,11 +1,13 @@
 package dungeonmania.movingEntity;
 
+import dungeonmania.DungeonInfo;
 import dungeonmania.player.Player;
 
 public interface MercenaryType {
     public boolean checkBribeAmoountEnough(int bribeAmount);
     public boolean checkBribeDistance(Player player);
     public boolean bribe(int bribeAmount);
+    public void mindControl(DungeonInfo dungeonInfo);
     public int getCostToBribe();
     public void setStrategy(MercenaryMovingStrategy strategy);
     public void setPlayerInvisibleStrategy(Player player);
